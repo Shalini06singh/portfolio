1369f77a5f6726cc9d2e571dfd09f23b479c83e9
Shalini06singh <singh0605shalini@gmail.com>
2024-09-18 21:17:01 +0530

    errors fixed again

--- src/app/components/Featured.tsx
+++ src/app/components/Featured.tsx
@@ -35,7 +35,7 @@ function Featured() {
       <div className="mt-10">
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 justify-center">
           {featuredProjects.map((project: Project) => (
-            <div className="flex justify-center" key={project.id}>
+            <div key={project.id} className="flex justify-center">
               <BackgroundGradient className="flex flex-col rounded-[22px] bg-white dark:bg-zinc-900 overflow-hidden h-full max-w-sm">
                 <div className="p-4 sm:p-6 flex flex-col items-center text-center flex-grow">
                   <p className="text-lg sm:text-xl text-black mt-4 mb-2 dark:text-neutral-200">
